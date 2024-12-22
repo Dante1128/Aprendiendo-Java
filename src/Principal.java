c0323d4475dd72372e3b67ca49f2417e2792003b
Dante1128 <danteyupanqui2811@gmail.com>
2024-12-22 12:19:51 -0400

    herencia

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import com.aluracursos.screenmach.modelos.Pelicula;
+import com.aluracursos.screenmach.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,6 +14,16 @@ public class Principal {
         miPelicula.evalua(7.8);
         System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon  = new Serie();
+        casaDragon.setNombre("Casa del Dragon");
+        casaDragon.setFecheDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+
+        System.out.println(casaDragon.getDuracionEnMinutos());
 
 
 
